2f9f187e7ac836ca9d79880d2825c5893bf4458d
ivan-kasianov <i.kaianovv@gmail.com>
2026-05-30 18:38:44 +0200

    homework_26 done and update gitignore

---
+++ homework/ivan_kasianov/homework_26/task_1.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.implicitly_wait(5)
+    chrome_driver.set_window_size(1920, 1080)
+    return chrome_driver
+
+
+def test_add_to_cart(driver):
+    driver.get("http://testshop.qa-practice.com/")
+    product = driver.find_element(
+        By.CSS_SELECTOR, '[alt="Customizable Desk"]'
+    )
+    product.click()
+    button_add_to_card = driver.find_element(
+        By.ID, "add_to_cart"
+    )
+    button_add_to_card.click()
+    button_continue_shopping = driver.find_element(
+        By.CLASS_NAME, "btn-secondary"
+    )
+    button_continue_shopping.click()
+    wait_second = WebDriverWait(driver, 3)
+    wait_second.until(
+        EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, ".my_cart_quantity"), "1"
+        )
+    )
+    button_card = driver.find_elements(
+        By.CLASS_NAME, "o_wsale_my_cart"
+    )
+    button_card[0].click()
+    assert "Customizable Desk" in driver.find_element(
+        By.CLASS_NAME, "d-inline"
+    ).text
