1bb2613d91c7ba4c09405375c9e51a2752778ecf
ivan-kasianov <i.kaianovv@gmail.com>
2026-05-31 22:55:58 +0200

    Added the option to open an item in a new tab

--- homework/ivan_kasianov/homework_26/task_1.py
+++ homework/ivan_kasianov/homework_26/task_1.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -18,7 +19,13 @@ def test_add_to_cart(driver):
     product = driver.find_element(
         By.CSS_SELECTOR, '[alt="Customizable Desk"]'
     )
-    product.click()
+    (ActionChains(driver)
+     .key_down(Keys.COMMAND)
+     .click(product)
+     .key_up(Keys.COMMAND)
+     .perform())
+    tabs = driver.window_handles
+    driver.switch_to.window(tabs[1])
     button_add_to_card = driver.find_element(
         By.ID, "add_to_cart"
     )
@@ -27,16 +34,27 @@ def test_add_to_cart(driver):
         By.CLASS_NAME, "btn-secondary"
     )
     button_continue_shopping.click()
-    wait_second = WebDriverWait(driver, 3)
+    wait_second = WebDriverWait(driver, 5)
     wait_second.until(
         EC.text_to_be_present_in_element(
             (By.CSS_SELECTOR, ".my_cart_quantity"), "1"
         )
     )
+    driver.close()
+    driver.switch_to.window(tabs[0])
     button_card = driver.find_elements(
         By.CLASS_NAME, "o_wsale_my_cart"
     )
     button_card[0].click()
+    wait = WebDriverWait(driver, 5)
+    wait.until(
+        EC.text_to_be_present_in_element((
+            By.CLASS_NAME,
+            "d-inline"
+        ),
+            "Customizable Desk (Steel, White)"
+        )
+    )
     assert "Customizable Desk" in driver.find_element(
         By.CLASS_NAME, "d-inline"
     ).text
